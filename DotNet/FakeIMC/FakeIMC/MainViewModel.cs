9305d860da32dd6e1445fed9719e8aeaca70c4ff
sebastiandymel <dymel@o2.pl>
2018-05-31 12:20:33 +0200

    SAVE SKIN COLOR TO CONFIGURATION STORAGE

--- DotNet/FakeIMC/FakeIMC/MainViewModel.cs
+++ DotNet/FakeIMC/FakeIMC/MainViewModel.cs
@@ -4,8 +4,12 @@ using MaterialDesignThemes.Wpf;
 using Reactive.Bindings;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
 using System.Reactive;
 using System.Reactive.Linq;
+using System.Reflection;
+using System.Windows;
 
 namespace FakeIMC
 {
@@ -59,11 +63,24 @@ namespace FakeIMC
         public SkinViewModel()
         {
             Swatches = new SwatchesProvider().Swatches;
+
+            var storage = new SkinConfigurationStorage(Swatches);
+
+            
             ApplyPrimaryCommand = new ReactiveCommand<Swatch>();
             ApplyPrimaryCommand.Subscribe(Observer.Create<Swatch>(s =>
             {
+                storage.Save(s);
                 new PaletteHelper().ReplacePrimaryColor(s);
             }));
+
+            // DEFAULT
+            var sw = storage.GetStoredSwatch();
+            if (sw != null)
+            {
+                new PaletteHelper().ReplacePrimaryColor(sw);
+            }
+            
 
             var light = false;
             var pallette = new PaletteHelper();
@@ -71,13 +88,69 @@ namespace FakeIMC
             LightDarkSwitch.Subscribe(() =>
             {
                 light = !light;
+                storage.Save(light);
                 pallette.SetLightDark(light);
             });
+            pallette.SetLightDark(storage.GetStoredIsDarkTheme());
         }
 
         public IEnumerable<Swatch> Swatches { get; set; }
         public ReactiveCommand LightDarkSwitch { get; set; }        
         public ReactiveCommand<Swatch> ApplyPrimaryCommand { get; }
+    }
+
+    internal class SkinConfigurationStorage
+    {
+        public SkinConfigurationStorage(IEnumerable<Swatch> swatches)
+        {
+            this.swatches = swatches;
+        }
+        private const string SchemeKey = "ColorScheme";
+        private const string DarkKey = "DarkThemeOn";
+        private readonly IEnumerable<Swatch> swatches;
+
+        internal Swatch GetStoredSwatch()
+        {
+            Configuration config = ConfigurationManager.OpenExeConfiguration(Assembly.GetExecutingAssembly().Location);
+
+            var name = config.AppSettings.Settings[SchemeKey]?.Value;
+
+            var swatch = swatches.FirstOrDefault(x => x.Name == name);
+            return swatch;
+        }
+
+        internal void Save(Swatch sw)
+        {
+            Configuration config = ConfigurationManager.OpenExeConfiguration(Assembly.GetExecutingAssembly().Location);
+
+            config.AppSettings.Settings.Remove(SchemeKey);
+            config.AppSettings.Settings.Add(SchemeKey, sw.Name);
+
+            config.Save(ConfigurationSaveMode.Modified);
+        }
+
+        internal void Save(bool isDark)
+        {
+            Configuration config = ConfigurationManager.OpenExeConfiguration(Assembly.GetExecutingAssembly().Location);
+
+            config.AppSettings.Settings.Remove(DarkKey);
+            config.AppSettings.Settings.Add(DarkKey, isDark.ToString());
+
+            config.Save(ConfigurationSaveMode.Modified);
+        }
+
+        internal bool GetStoredIsDarkTheme()
+        {
+            Configuration config = ConfigurationManager.OpenExeConfiguration(Assembly.GetExecutingAssembly().Location);
+
+            var isOn = config.AppSettings.Settings[DarkKey]?.Value;
+            if (bool.TryParse(isOn, out var isOnVal))
+            {
+                return isOnVal;
+            }
+            return false;
+        }
+
     }
 
     public class LogItem
