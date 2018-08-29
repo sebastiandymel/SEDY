e3e8c2bd220ba1fe593067d7d9959c92e825e1c3
sebastiandymel <sedy@dgs.com>
2018-08-29 10:16:48 +0200

    script

--- PowerShell/CountLargeFiles.ps1
+++ PowerShell/CountLargeFiles.ps1
@@ -1,9 +1,11 @@
-Get-ChildItem . -Filter "*.cs" -Recurse |
-    Where-Object { 
-        ($_.FullName -notcontains "obj") -and
-                 
-        ((  
-        Get-Content $_.FullName |
-         Measure-Object -Line |
-         Select-Object -ExpandProperty Lines) -gt 2000)
-    } | Out-File largeFiles.txt
+    Get-ChildItem -re -in *.cs |  
+    Where-Object{ -not $_.PSIsContainer }| 
+    Where-Object{$_.FullName -notmatch 'obj'} |
+    Where-Object{$_.FullName -notmatch 'Test'} |  
+    Where-Object{$_.FullName -notmatch 'GlobalSuppression'} |  
+    Where-Object{$_.FullName -notmatch 'Resource.Designer'} | 
+    Where-Object {
+                 $(Get-Content $_.FullName |
+                  Measure-Object -Line).Lines -gt 1000   }|
+    Sort-Object Length -descending |   
+    Select-Object FullName -first 1000 > bigcsfiles.txt
