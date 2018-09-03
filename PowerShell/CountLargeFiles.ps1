98a76c4453ffccc1a81f78731901be328d99d0e5
sebastiandymel <sedy@dgs.com>
2018-09-03 09:22:30 +0200

    aa

--- PowerShell/CountLargeFiles.ps1
+++ PowerShell/CountLargeFiles.ps1
@@ -1,11 +1,11 @@
-    Get-ChildItem -re -in *.cs |  
-    Where-Object{ -not $_.PSIsContainer }| 
-    Where-Object{$_.FullName -notmatch 'obj'} |
-    Where-Object{$_.FullName -notmatch 'Test'} |  
-    Where-Object{$_.FullName -notmatch 'GlobalSuppression'} |  
-    Where-Object{$_.FullName -notmatch 'Resource.Designer'} | 
-    Where-Object {
-                 $(Get-Content $_.FullName |
-                  Measure-Object -Line).Lines -gt 1000   }|
-    Sort-Object Length -descending |   
-    Select-Object FullName -first 1000 > bigcsfiles.txt
+(Get-ChildItem -re -in *.cs |  
+Where-Object{ -not $_.PSIsContainer }| 
+Where-Object{$_.FullName -notmatch 'obj'} |
+Where-Object{$_.FullName -notmatch 'Test'} |  
+Where-Object{$_.FullName -notmatch 'GlobalSuppression'} |  
+Where-Object{$_.FullName -notmatch 'Resource.Designer'} | 
+ForEach-Object { @{ File=$_.FullName; Lines= $(Get-Content $_.FullName |  Measure-Object -Line).Lines } }|
+Where-Object { $_.Lines  -gt 1000   }|
+Select-Object -first 1000).ForEach({[PSCustomObject]$_}) |
+Sort-Object -Property Lines -Descending |
+Format-Table -AutoSize > bigcsfiles.txt
