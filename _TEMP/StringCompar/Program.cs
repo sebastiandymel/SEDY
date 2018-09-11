e262098d2fb241881729018fa4b93ef1f064318f
sebastiandymel <dymel@o2.pl>
2018-09-11 21:14:26 +0200

    string compare

--- _TEMP/StringCompar/Program.cs
+++ _TEMP/StringCompar/Program.cs
@@ -16,6 +16,10 @@ namespace StringCompar
                 "String.Something.Format(x,y)",
                 "String.Something.Format(x,y)A",
                 "String.Something.Format(x,y",
+                "String.Something.Format{x,y}A",
+                "String.Something{0}.Format(x,y",
+                "{0.00}",
+                "{}}",
             };
 
             foreach (var item in items)
@@ -23,12 +27,26 @@ namespace StringCompar
                 Console.WriteLine($"{Compare(item)} \t = {item}");
             }
 
+            Console.WriteLine("============================");
+
+            foreach (var item in items)
+            {
+                Console.WriteLine($"{Compare2(item)} \t = {item}");
+            }
+
             Console.Read();
+        }
+
+        private static bool Compare2(string input)
+        {
+            var regExp = @"\{.*\}";
+            var matches = Regex.Match(input, regExp);
+            return matches.Success;
         }
 
         private static bool Compare(string input)
         {
-            var regExp = @".Format\(.*\)";
+            var regExp = @"^.*.Format\(.*\)$";
             var matches = Regex.Match(input, regExp);
             return matches.Success;
         }
