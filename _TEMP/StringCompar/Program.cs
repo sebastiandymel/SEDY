a8f5f6933f95103f5469aca8f321df2a2b65400d
sebastiandymel <dymel@o2.pl>
2018-09-11 18:29:34 +0200

    string compare

---
+++ _TEMP/StringCompar/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace StringCompar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var items = new [] 
+            {
+                "String.AlaMaKota",
+                "String.Something.Format(x)",
+                "String.Something.Format(x,y)",
+                "String.Something.Format(x,y)A",
+                "String.Something.Format(x,y",
+            };
+
+            foreach (var item in items)
+            {
+                Console.WriteLine($"{Compare(item)} \t = {item}");
+            }
+
+            Console.Read();
+        }
+
+        private static bool Compare(string input)
+        {
+            var regExp = @".Format\(.*\)";
+            var matches = Regex.Match(input, regExp);
+            return matches.Success;
+        }
+    }
+}
