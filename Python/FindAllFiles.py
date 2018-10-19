653eb8438d2b38efa2000f41d430e0ec189a610b
Sebastian Dymel <sedy@dgs.com>
2018-10-19 09:12:50 +0200

    updates

--- Python/FindAllFiles.py
+++ Python/FindAllFiles.py
@@ -3,9 +3,13 @@ import re
 from os.path import expanduser
 listOfFiles = []
 for root, directories, filenames in os.walk(r'C:\\GIT\\Phoenix\\Integration\\Application\UserInterface\\Oasis\\BrandViews'):
+#for root, directories, filenames in os.walk(r'C:\\GIT\\Phoenix\\Integration\\Application\UserInterface\\GenieMedical\\GenieMedicalViews'):
+#for root, directories, filenames in os.walk(r'C:\\GIT\\Phoenix\\Integration\\Application\UserInterface\\Oasis\\BrandViews'):
     for filename in filenames:           
-     if re.match("(.*)EF(.*)View.xaml$", filename): 
-     #if re.match("(.*)View.xaml$", filename): 
+        #if (re.match("(.*)Medical(.*)View.xaml$", filename) or re.match("(.*)Medical(.*)ViewModel.cs$", filename) or re.match("(.*)Medical(.*)Module.cs$", filename)): 
+        if re.match("(.*)EF(.*)ViewModel.cs$", filename): 
+        #if re.match("(.*)EF(.*)View.xaml$", filename): 
+        #if re.match("(.*)View.xaml$", filename): 
                 listOfFiles.append(filename)
 
 print (*listOfFiles, sep = "\n")
