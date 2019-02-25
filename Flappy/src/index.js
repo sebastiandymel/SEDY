a54fa4191ee436e2f2dd3087305742e9038b0c6e
sebastiandymel <dymel@o2.pl>
2019-02-25 19:17:00 +0100

    flappy bird js

--- Flappy/src/index.js
+++ Flappy/src/index.js
@@ -11,6 +11,9 @@ var ctx = null;
 var bars = [];
 var player_y = 100;
 var isUp = false;
+var colision = false;
+var player_center_x = 100;
+
 window.onload = load;
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
@@ -21,11 +24,12 @@ function load() {
   canvas.setAttribute("height", 480);
   ctx = canvas.getContext("2d");
 
-  var offset = canvas.width / 5;
+  var numberOfBarsPerScreen = 5;
+  var offset = canvas.width / numberOfBarsPerScreen;
 
-  for (var i = 0; i < 5; i++) {
+  for (var i = 0; i < numberOfBarsPerScreen; i++) {
     bars[i] = {
-      x: offset * i + canvas.width / 3,
+      x: offset * i + canvas.width / 2,
       gap: 30 * (i + 1)
     };
   }
@@ -34,6 +38,10 @@ function load() {
 }
 
 function draw() {
+  if (colision) {
+    return;
+  }
+
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
   var gapSize = player_height * 3.5;
@@ -62,30 +70,72 @@ function draw() {
     ctx.fillStyle = "green";
     ctx.fill();
     ctx.closePath();
+
+    var r1 = toRect(x, y, width, height);
+    var r2 = toRect(x, height + gapSize, width, bars[i].gap);
+    var playerR = playerRect();
+    colision = colision || hasCollide(playerR, r1) || hasCollide(playerR, r2);    
   }
 
-  // DRAW PLAYER
-  if (isUp) {
-    player_y -= player_speed * 5;
-  } else {
-    player_y += player_speed;
+  // UPDATE PLAYER POSITION
+  if (!colision) {
+    if (isUp) {
+      player_y -= player_speed * 5;
+    } else {
+      player_y += player_speed;
+    }
   }
 
   ctx.beginPath();
-  ctx.arc(100, player_y, player_height / 2, 0, Math.PI * 2);
+  ctx.arc(player_center_x, player_y, player_height / 2, 0, Math.PI * 2);
   ctx.fillStyle = "black";
   ctx.fill();
   ctx.closePath();
 }
 
 function keyDownHandler(e) {
-  if (e.key == "W" || e.key == "w") {
+  if (isKeyUp(e)) {
     isUp = true;
   }
 }
 
 function keyUpHandler(e) {
-  if (e.key == "W" || e.key == "w") {
+  if (isKeyUp(e)) {
     isUp = false;
   }
+}
+
+const spaceKeycode = 32;
+function isKeyUp(e) {
+  if (e.key == "W" || e.key == "w" || e.keyCode == spaceKeycode) {
+    return true;
+  }
+  return false;
+}
+
+function hasCollide(r1, r2) {
+  return !(
+    r2.left > r1.right ||
+    r2.right < r1.left ||
+    r2.top > r1.bottom ||
+    r2.bottom < r1.top
+  );
+}
+
+function playerRect() {
+  return toRect(
+    player_center_x - player_height / 2,
+    player_y - player_height / 2,
+    player_height,
+    player_height
+  );
+}
+
+function toRect(x, y, width, height) {
+  return {
+    left: x,
+    right: x + width,
+    top: y,
+    bottom: y + height
+  };
 }
