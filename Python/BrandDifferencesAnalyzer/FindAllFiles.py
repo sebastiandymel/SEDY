0d1d38e5587b452917b83dc5457e183d5685e9d7
Sebastian Dymel <dymel@o2.pl>
2019-09-24 12:11:52 +0200

    pscript

---
+++ Python/BrandDifferencesAnalyzer/FindAllFiles.py
@@ -0,0 +1,65 @@
+import os
+import re
+from os.path import expanduser
+from datetime import datetime
+
+def Analyze(title: str,dirPath: str, regPattern: str):
+    """
+        Analyze given directory for specific regex path files \n
+        List all the files and append to output.txt.
+    """
+    listOfFiles = []
+    for root, directories, filenames in os.walk(dirPath):
+        for filename in filenames:
+            if re.match(regPattern, filename): 
+                listOfFiles.append(filename)
+    print(title)
+    print (*listOfFiles, sep = "\n")
+    
+    # Prepare list for writing
+    for i in range(len(listOfFiles)):
+        listOfFiles[i] = listOfFiles[i] + '\n'
+    # Write to file
+
+    shortDate = datetime.today().strftime('%Y-%m-%d')
+    f = open(shortDate + "-output.txt", "a")
+    f.write('\n' + title + '\n')
+    f.writelines(listOfFiles)
+
+# EXPRESSFIT
+pathToBrandViews = r'C:\\GIT\\Phoenix\\Integration\\Application\UserInterface\\Oasis\\BrandViews'
+Analyze(
+    "### ExpressFit specific ViewModels", 
+    pathToBrandViews, 
+    "(.*)EF(.*)ViewModel.cs$"
+    )
+Analyze(
+    "### ExpressFit specific Views", 
+    pathToBrandViews, 
+    "(.*)EF(.*)View.xaml$"
+    )
+
+# GENIE MEDICAL
+pathToGenieMedicalViews = r'C:\\GIT\\Phoenix\\Integration\\Application\UserInterface\\GenieMedical\\GenieMedicalViews'
+Analyze(
+    "### Medical specific ViewModels", 
+    pathToGenieMedicalViews, 
+    "(.*)Medical(.*)View.xaml$"
+    )
+Analyze(
+    "### Medical specific Views", 
+    pathToGenieMedicalViews, 
+    "(.*)Medical(.*)ViewModel.cs$"
+    )
+
+# HEAR SUITE
+Analyze(
+    "### HearSuite specific ViewModels", 
+    pathToBrandViews,
+    "(.*)HS(.*)ViewModel.cs$"
+    )
+Analyze(
+    "### HearSuite specific Views", 
+    pathToBrandViews,
+    "(.*)HS(.*)View.xaml$"
+    )
